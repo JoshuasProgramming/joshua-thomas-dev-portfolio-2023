588a8d7d0a06f6d8b4e2d8b6d5afbbc2e37b658a
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-04 17:54:24 +0000

    Created the Hero section with appropriate styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,8 @@ import '../styles/style.css';
 export default function Hero() {
   return (
     <section className="hero-section">
-        <p>Hey I'm <span>Joshua Thomas</span></p>
-        <p className='purple-text'>a Full Stack Engineer</p>
-        <p>I enjoy creating sleek and responsive applications, giving the user the best experience possible.</p>
+        <p>Hey I'm <span>Joshua Thomas</span> <br></br> <div className='purple-text'>a Full Stack Engineer</div></p>
+        <p>I enjoy creating sleek and responsive applications<br></br>giving the user the best experience possible.</p>
     </section>
   )
 }
