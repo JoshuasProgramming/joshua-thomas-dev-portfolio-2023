224c3158a9d4a83d0f7e5615381e392ced5febdf
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-05 11:11:22 +0000

    added php icon

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import ReactIcon from '../icons/react_icon.png';
 import JS from '../icons/javascript.png';
 import json from '../icons/json.png';
+import php from '../icons/php.png';
 import Sass from '../icons/sass.png';
 import Css from '../icons/css.png';
 import Html from '../icons/html.png';
@@ -19,6 +20,7 @@ class Languages extends Component {
             {images: {src: ReactIcon, alt: 'react-image'}, name: 'react'},
             {images: {src: JS, alt: 'js-image'}, name: 'javascript (ES6 / ES7)'},
             {images: {src: json, alt: 'json-image'}, name: 'json'},
+            {images: {src: php, alt: 'php-image'}, name: 'php'},
             {images: {src: Sass, alt: 'sass-image'}, name: 'SASS / SCSS'},
             {images: {src: Css, alt: 'css-image'}, name: 'CSS'},
             {images: {src: Html, alt: 'html-image'}, name: 'HTML'}
