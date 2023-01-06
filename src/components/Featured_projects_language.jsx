55968f520345e4462ed773bbb1a339294cb11067
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-06 18:48:47 +0000

    Added condition for langauge5 prop

--- src/components/Featured_projects_language.jsx
+++ src/components/Featured_projects_language.jsx
@@ -8,7 +8,8 @@ const Featured_projects_language = (props) => {
         <div><h3>{props.language2}</h3></div>
         <div><h3>{props.language3}</h3></div>
         <div><h3>{props.language4}</h3></div>
-        <div><h3>{props.language5}</h3></div>
+        {props.language5 != undefined ?  <div><h3>{props.language5}</h3></div> : false}
+       
     </div>
     )
 }
