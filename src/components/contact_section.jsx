a637dba772d240e3e08740edc38b13146f804c3d
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-08 01:15:56 +0000

    added text above contact me section

--- src/components/contact_section.jsx
+++ src/components/contact_section.jsx
@@ -7,6 +7,9 @@ import '../styles/style.css';
 export default function contact_section() {
     return (
       <section className="hero-section contact">
+          <div className="top">
+            <p>Contact me &#8650;</p>
+          </div>
           <p><div className='purple-text'>Get In Touch</div></p>
           <p className='text'>If you would like to discuss your project further or have any questions, please don't hesitate to contact me.</p>
     
