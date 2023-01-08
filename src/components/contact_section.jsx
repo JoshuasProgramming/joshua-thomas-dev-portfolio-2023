3e5f7fb43e9ca9811f242f3cda9ff5d3f541ccc3
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-08 01:06:38 +0000

    Changed the text layout for 'text' class

--- src/components/contact_section.jsx
+++ src/components/contact_section.jsx
@@ -8,7 +8,7 @@ export default function contact_section() {
     return (
       <section className="hero-section contact">
           <p><div className='purple-text'>Get In Touch</div></p>
-          <p className='text'>If you would like to discuss your project further or have any questions, <br></br>please don't hesitate to contact me.</p>
+          <p className='text'>If you would like to discuss your project further or have any questions, please don't hesitate to contact me.</p>
     
           <div className="cv-container">
             <a>Email Me</a>
