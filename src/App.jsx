7bfeb8b2c5e8f77577f7283a7f15ff1d431e0b6c
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-08 17:27:08 +0000

    redirected to home page

--- src/App.jsx
+++ src/App.jsx
@@ -11,45 +11,57 @@ import Contact_section from './components/contact_section';
 //image imports
 import nightMode from './images/nightmode.svg';
 
+//import react router
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+//import pages
+import Project_page from './pages/Project_page';
+import Home from './pages/Home';
 
 function App() {
   return (
     // <Projects />
   
     <>
-
       <Router>
-      <nav>
-        <ul className="navigation">
-            <div className="left">
-                <h1><span>Joshua</span>Thomas.</h1>
-            </div>
-
-            <div className="right">
-
-              <Link to="pages/projects">Projects</Link>
-              <Link to="pages/blog">Blog</Link>
-              <Link to="pages/about">About</Link>
-
-              <div className="night-light-mode-icon">
-                  <img src={nightMode} alt="nightmode-icon" />
+        <nav>
+          <ul className="navigation">
+              <div className="left">
+                <h1><Link to="/"><span>Joshua</span></Link>Thomas.</h1>
               </div>
-            </div>
-        </ul>
-      </nav>
+
+              <div className="right">
+
+                <Link to="pages/projects_page">Projects</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/about">About</Link>
+
+                <div className="night-light-mode-icon">
+                    <img src={nightMode} alt="nightmode-icon" />
+                </div>
+              </div>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+
+        <Routes>
+          <Route path="pages/project_page" element={ <Project_page/> }/>
+        </Routes>
 
       </Router>
 
       
-      <Hero />
+      {/* <Hero />
       <Languages/>
       <Featured_projects_heading/>
       <Featured_projects/>
       <Projects />
       <View_All_projects_btn/>
-      <Contact_section/>
+      <Contact_section/> */}
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Projects from '../components/Projects';
+import Hero from '../components/Hero';
+import Languages from '../components/Languages';
+import Featured_projects_heading from '../components/Featured_projects_heading';
+import Featured_projects from '../components/Featured_projects';
+import View_All_projects_btn from '../components/View_All_projects_btn';
+import Contact_section from '../components/contact_section';
+
+function Home() {
+  return (
+    // <Projects />
+  
+    <> 
+      <Hero />
+      <Languages/>
+      <Featured_projects_heading/>
+      <Featured_projects/>
+      <Projects />
+      <View_All_projects_btn/>
+      <Contact_section/>
+    </>
+  );
+}
+
+export default Home;
