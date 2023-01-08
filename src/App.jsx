6e251c7badc623573c4c90b276a5e1e9bfa48fc2
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-08 00:50:26 +0000

    Added contact component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Languages from './components/Languages';
 import Featured_projects_heading from './components/Featured_projects_heading';
 import Featured_projects from './components/Featured_projects';
 import View_All_projects_btn from './components/View_All_projects_btn';
+import Contact_section from './components/contact_section';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Featured_projects/>
       <Projects />
       <View_All_projects_btn/>
+      <Contact_section/>
     </>
   );
 }
---
+++ src/components/contact_section.jsx
@@ -0,0 +1,18 @@
+//react imports 
+import React, { Component } from 'react';
+
+//style imports
+import '../styles/style.css';
+
+export default function contact_section() {
+    return (
+      <section className="hero-section contact">
+          <p><div className='purple-text'>Get In Touch</div></p>
+          <p className='text'>If you would like to discuss your project further or have any questions, <br></br>please don't hesitate to contact me.</p>
+    
+          <div className="cv-container">
+            <a>Email Me</a>
+          </div>
+      </section>
+    )
+}
