b1fd83c7adbe3365840db498d93b99ff1cc6d631
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-10 01:18:42 +0000

    updated languages condition

--- src/components/Featured_projects_language.jsx
+++ src/components/Featured_projects_language.jsx
@@ -4,10 +4,10 @@ const Featured_projects_language = (props) => {
 
     return (
     <div className="featured-projects-language-container"> 
-        <div><h3>{props.language1}</h3></div>
-        <div><h3>{props.language2}</h3></div>
-        <div><h3>{props.language3}</h3></div>
-        <div><h3>{props.language4}</h3></div>
+        {props.language1 != undefined ?  <div><h3>{props.language1}</h3></div> : false}
+        {props.language2 != undefined ?  <div><h3>{props.language2}</h3></div> : false}
+        {props.language3 != undefined ?  <div><h3>{props.language3}</h3></div> : false}
+        {props.language4 != undefined ?  <div><h3>{props.language4}</h3></div> : false}
         {props.language5 != undefined ?  <div><h3>{props.language5}</h3></div> : false}
        
     </div>
