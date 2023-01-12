0251191e931e42c0a0981f1c870d7bd0cf5a190e
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-12 22:05:01 +0000

    removed loads of unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import React from 'react'
-import Projects from './components/Projects';
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import Languages from './components/Languages';
-import Featured_projects_heading from './components/Featured_projects_heading';
-import Featured_projects from './components/Featured_projects';
-import View_All_projects_btn from './components/View_All_projects_btn';
-import Contact_section from './components/contact_section';
 
 //image imports
 import nightMode from './images/nightmode.svg';
