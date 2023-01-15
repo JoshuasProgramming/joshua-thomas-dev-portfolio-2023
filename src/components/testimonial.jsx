d792776f129f139f1a3ef49ded3e7fc6496c2fa5
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-15 17:19:31 +0000

    Half way through testimonials section

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -4,12 +4,10 @@ import testimonial1 from '../testimonial_images/testimonial 1.png';
 
 function testimonials() {
   return (
-    <div className="hero-section">
-        <div className="top">
-            <p>Testimonials</p>
-            <img src={testimonial1} alt="testimonial1" />
-            <p className="text">Creating my website with Joshua was effortless, he exceeded my aesthetic expectations and was responsive to my requests. Transfer of files was smooth, allowing me to launch my website quickly. Joshua is highly professional and easy to work with.</p>
-        </div>
+    <div className="testimonials">
+        <p>Testimonials</p>
+        <img src={testimonial1} alt="testimonial1" />
+        <p className="text">Creating my website with Joshua was effortless, he exceeded my aesthetic expectations and was responsive to my requests. Transfer of files was smooth, allowing me to launch my website quickly. Joshua is highly professional and easy to work with.</p>
     </div>
   )
 }
