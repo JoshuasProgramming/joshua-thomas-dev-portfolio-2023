23dc053c744f2d63ed7789f5339b91ced2c8c077
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-15 17:27:47 +0000

    Completed testimonials

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -7,6 +7,9 @@ function testimonials() {
     <div className="testimonials">
         <p>Testimonials &#8650;</p>
         <img src={testimonial1} alt="testimonial1" />
+        <div className="testimonials-info">
+        <h3>Tarrazy Guemri</h3>
+        </div>
         <p className="text">Creating my website with Joshua was effortless, he exceeded my aesthetic expectations and was responsive to my requests. Transfer of files was smooth, allowing me to launch my website quickly. Joshua is highly professional and easy to work with.</p>
     </div>
   )
