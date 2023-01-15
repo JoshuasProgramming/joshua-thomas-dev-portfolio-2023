76deaff032cec0bba54cb66052ae7716830c859a
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-15 17:20:34 +0000

    Added two arrows next to testimonials

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -5,7 +5,7 @@ import testimonial1 from '../testimonial_images/testimonial 1.png';
 function testimonials() {
   return (
     <div className="testimonials">
-        <p>Testimonials</p>
+        <p>Testimonials &#8650;</p>
         <img src={testimonial1} alt="testimonial1" />
         <p className="text">Creating my website with Joshua was effortless, he exceeded my aesthetic expectations and was responsive to my requests. Transfer of files was smooth, allowing me to launch my website quickly. Joshua is highly professional and easy to work with.</p>
     </div>
