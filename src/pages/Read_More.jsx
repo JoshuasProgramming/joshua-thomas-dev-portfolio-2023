fbbe663582c03aa77716ec478925e69ebadc42f0
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-18 16:43:12 +0000

    Added title and image to read more + styling

--- src/pages/Read_More.jsx
+++ src/pages/Read_More.jsx
@@ -9,7 +9,7 @@ export default function Read_More(props) {
   return (
     <section className="read-more-container">
       <div className="read-more-title">
-        <h1>{location.state.title}</h1>
+        <h1><span>{location.state.title}</span> - An Overview</h1>
       </div>
       <div className="read-more-image">
         <img src={location.state.image} alt={location.state.alt}/>
