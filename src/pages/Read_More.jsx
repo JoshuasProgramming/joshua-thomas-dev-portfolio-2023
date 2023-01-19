e3dc8ac3022e3efaa8a4b90ff9eeee6ea520fac3
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-19 11:04:03 +0000

    Added heading for read_more snippet

--- src/pages/Read_More.jsx
+++ src/pages/Read_More.jsx
@@ -25,6 +25,11 @@ export default function Read_More(props) {
       <div className="read-more-paragraph1">
         <p>{location.state.paragraph2}</p>
       </div>
+      
+      <div className="read-more-heading">
+        <h1>{location.state.heading1}</h1>
+      </div>
+
       <div className="read-more-snippet">
         <img src={location.state.code_snippet1} alt={location.state.code_snippet1 + "IMG"}/>
       </div>
